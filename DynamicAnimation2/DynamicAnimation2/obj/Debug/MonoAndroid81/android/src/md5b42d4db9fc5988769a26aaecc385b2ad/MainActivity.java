503d00ed0b9e621611eec5ea8e8e860d8b9afdb2
Tamara Bozhanova <tamara.bozhanova@gmail.com>
2018-11-06 21:09:51 +0200

    added dynamic animation

---
+++ DynamicAnimation2/DynamicAnimation2/obj/Debug/MonoAndroid81/android/src/md5b42d4db9fc5988769a26aaecc385b2ad/MainActivity.java
@@ -0,0 +1,47 @@
+package md5b42d4db9fc5988769a26aaecc385b2ad;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("DynamicAnimation2.MainActivity, DynamicAnimation2", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("DynamicAnimation2.MainActivity, DynamicAnimation2", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
